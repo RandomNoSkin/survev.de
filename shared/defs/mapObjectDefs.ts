a088c9e131a82257461d5384266a247abcbe2aea
Neel Sreenivasa <neelsreenivasa@gmail.com>
2026-08-01 22:37:51 -0700

    pre hosting commit

--- shared/defs/mapObjectDefs.ts
+++ shared/defs/mapObjectDefs.ts
@@ -1678,7 +1678,7 @@ function createStone<T extends ObstacleDef>(e: Partial<T>): T {
         reflectBullets: false,
         hitParticle: "rockChip",
         explodeParticle: "rockBreak",
-        loot: [{ type: "scout_elite", count: 1 }],
+        loot: [],
         map: { display: true, color: 0xb3b3b3, scale: 1 },
         terrain: { grass: true, beach: false, riverShore: true },
         img: {
@@ -1862,7 +1862,7 @@ function createTree<T extends ObstacleDef>(e: Partial<T>): T {
         explodeParticle: "woodLog",
         reflectBullets: false,
         isTree: true,
-        loot: [autoLoot("henry", 1)],
+        loot: [],
         map: { display: true, color: 0x3e502e, scale: 2.5 },
         terrain: { grass: true, beach: false },
         img: {
